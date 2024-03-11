437eab2d29c98f40629f2f4ac932322f1268e035
ZackHu <hu.zixi@northeastern.edu>
2024-03-11 16:34:58 -0700

    feat: connect to firebase backend

---
+++ src/app/firebase/firebase.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8ts14MgT3qn0lY_6kNlnZ1VVhFOuC7x4",
+  authDomain: "tetris-1c4c3.firebaseapp.com",
+  projectId: "tetris-1c4c3",
+  storageBucket: "tetris-1c4c3.appspot.com",
+  messagingSenderId: "143860219675",
+  appId: "1:143860219675:web:2afac54001081bb56a1dbe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
