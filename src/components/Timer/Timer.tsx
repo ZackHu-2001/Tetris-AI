3aee78f5dc760d14bf8dda19ae378600b32454d0
ZackHu <hu.zixi@northeastern.edu>
2024-03-14 14:44:00 -0700

    build: set up Timer component
    The updated Timer would start, pause and reset based on the game status.

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,37 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import { gameStateAtom } from '@/app/atoms/gameStateAtom';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { create } from 'zustand';
+import { useGameState } from '../gameVariable';
+
+interface timer {
+    time: number,
+    start: () => void,
+    pause: () => void,
+    reset: () => void,
+}
 
 type TimerProps = {
     
 };
 
 const Timer:React.FC<TimerProps> = () => {
+    const { gameState } = useGameState();
     const [time, setTime] = useState(0);
-    // const setGameState = useSetRecoilState(gameStateAtom);
-    // const gameState = useRecoilValue(gameStateAtom);
 
     const formatTime = (time: number): string => {
-        const hours = Math.floor(time / 3600);
-        const minutes = Math.floor((time - hours * 3600) / 60);
-        const seconds = time % 60;
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        const milliseconds = Math.floor((time % 1) / 0.01);
 
-        return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+        return `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}:${milliseconds < 10 ? '0' + milliseconds : milliseconds}`;
     }
 
-    // useEffect(() => {
-    //     let intervalId: NodeJS.Timeout; 
+    useEffect(() => {
+        let intervalId: NodeJS.Timeout | undefined; 
     
-    //     if (gameState.status == 'playing') { // Checking if showTimer is true
-    //         intervalId = setInterval(() => { // Setting up an interval if showTimer is true
-    //             setTime((time) => time + 1); // Incrementing the time state by 1 second
-    //         }, 1000); // Interval set to run every 1000 milliseconds (1 second)
-    //     }
+        if (gameState.status == 'playing') { // Checking if showTimer is true
+            intervalId = setInterval(() => { // Setting up an interval if showTimer is true
+                setTime((time) => time + 0.01); // Incrementing the time state by 1 second
+            }, 10); // Interval set to run every 1000 milliseconds (1 second)
+        }
+        
+        if (gameState.status == 'paused') {
+            if (intervalId !== undefined) {
+                clearInterval(intervalId);
+            }
+        }
+        
+        if (gameState.status == 'gameover') {
+            setTime(0);
+        }
+
+        return () => clearInterval(intervalId); // Cleanup function to clear the interval when the component unmounts or showTimer changes
+    }, [gameState, setTime]); // Dependency array - useEffect will re-run whenever showTimer changes
     
-    //     return () => clearInterval(intervalId); // Cleanup function to clear the interval when the component unmounts or showTimer changes
-    // }, [gameState.status == 'playing']); // Dependency array - useEffect will re-run whenever showTimer changes
     
-    
-    return <div>{formatTime(time)}</div>
+    return <div className='w-full flex '>{formatTime(time).split('').map((char: string, index: number) => {
+        return <div style={{width: '8rem'}} className=' flex justify-center'>{char}</div>
+    })}</div>
 }
 export default Timer;
