2249cfb18c884d68f21926815ab06e3db2e6b22f
kai-zong <zong.k@northeastern.edu>
2024-03-16 18:50:50 -0700

    fixed the border issue between blocks of a tetromino, to make it single border for adjacent blocks

--- src/components/NextPanel/NextPanel.tsx
+++ src/components/NextPanel/NextPanel.tsx
@@ -22,27 +22,35 @@
 // export default NextPanel;
 
 import React from 'react';
-import { useGameBoard } from '@/components/GameBoard'; // Ensure correct path
+import { useGameBoard, tetrominos} from '@/components/GameBoard'; // Ensure correct path
+
+
+// const binaryToTypeMapping = Object.entries(tetrominos).reduce((acc, [type, shapes]) => {
+//     // Assuming shapes are unique across types, which might need verification
+//     shapes.forEach(shape => {
+//         // The key is the binary representation as a string; value is the type
+//         acc[shape.toString()] = type;gut pull
+//     });
+//     return acc;
+// }, {});
 
 // Annotating the `filled` prop type directly in the parameter list
 const TetrominoBlock = ({ filled }: { filled: boolean }) => (
-    <div className={`w-4 h-4 ${filled ? 'bg-red-500' : 'bg-transparent'} border border-gray-200`}></div>
+    <div className={`w-4 h-4 m-[-1px] ${filled ? 'bg-red-500' : 'bg-transparent'} border border-gray-200`}></div>
 );
 
 
 const NextTetromino = ({ tetromino }: { tetromino: number[] }) => {
-    const cols = 4; // Assuming a standard width for tetrominos
+    const cols = 4;
     return (
-        <div className='flex flex-col items-center mb-2'>
+        <div className='flex flex-col items-center mb-2 p-[1px]'> {/* Add slight padding */}
             {tetromino.map((row, rowIndex) => (
                 <div key={rowIndex} className='flex'>
                     {Array.from({ length: cols }).map((_, colIndex) => {
-                        // Determine if the cell is filled
                         const isFilled = !!(row & (1 << (cols - 1 - colIndex)));
-                        // Only render TetrominoBlock for filled cells
                         return isFilled ? (
                             <TetrominoBlock key={colIndex} filled={true} />
-                        ) : null; // Return null for empty cells, so they don't get rendered
+                        ) : null;
                     })}
                 </div>
             ))}
