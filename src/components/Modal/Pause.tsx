95defd6f94fbeea6e15064e8a27e367fe3126dd1
ZackHu <hu.zixi@northeastern.edu>
2024-03-18 01:29:13 -0700

    Feat: support retry game

--- src/components/Modal/Pause.tsx
+++ src/components/Modal/Pause.tsx
@@ -1,28 +1,57 @@
 import React from 'react';
 import MenuButton from '../MenuButton/MenuButton';
 import { useGameState } from '../GameState';
+import { useGameBoard } from '../GameBoard';
+import { getRandomTetromino } from '@/components/GameBoard'
 
 type PauseProps = {};
 
 const Pause: React.FC<PauseProps> = () => {
-    const { setStatus, setModal } = useGameState();
+    const { setStatus, setModal, setMode } = useGameState();
+    const { setIsNewGame, append, setFallingTetromino, initializeGameBoard } = useGameBoard();
     const handleReturn = () => {
         setStatus("playing")
         setModal(null)
     }
 
     const handleRetry = () => {
+        setStatus("playing")
+        setModal(null)
+        
+        // reset timer
+        setIsNewGame(true)
 
+        // reset game board
+        // initialize nextTetrominoQueue
+        for (let i = 0; i < 6; i++) {
+            append(getRandomTetromino());
+        }
+
+        // initialize game board
+        initializeGameBoard();
+
+        // initialize fallingTetromino
+        setFallingTetromino(getRandomTetromino());
+
+        // change global game state
+        setStatus('playing');
+        setMode('sprint');
+        setModal(null);
+
+        // initialize the game board and add tetromino inside it
+        initializeGameBoard();
+        setFallingTetromino(getRandomTetromino())
     }
 
     const handleClickMainMenu = () => {
         setStatus(null)
         setModal('mainMenu')
         // reset timer
+        setIsNewGame(true)
 
     }
 
-    return <div style={{fontSize: '5rem', fontWeight: 'bold'}} className='w-full h-full flex flex-col justify-center items-center'>
+    return <div style={{ fontSize: '5rem', fontWeight: 'bold' }} className='w-full h-full flex flex-col justify-center items-center'>
         <div>Paused</div>
         <MenuButton text='Return' onClick={handleReturn}></MenuButton>
         <MenuButton text='Retry' onClick={handleRetry}></MenuButton>
