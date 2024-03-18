7094099e13588918a0a916832508c52d0a420709
ZackHu <hu.zixi@northeastern.edu>
2024-03-17 17:32:03 -0700

    Feat: support reflective layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,78 @@ import MainMenuModal from "../components/Modal/MainMenu";
 import StatusPanel from "@/components/StatusPanel/StatusPanel";
 import PlayField from "@/components/PlayField/PlayField";
 import NextPanel from "@/components/NextPanel/NextPanel";
-import { useState } from "react";
 import BasicModal from "@/components/Modal/BasicModal";
-import { GameState } from "@/components/GameState";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+  const [dimensions, setDimensions] = useState({
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0
+  })
 
-  const [gameState, setGameState] = useState<GameState>({
-    status: 'paused',
-    mode: null,
-    modal: 'mainMenu'
-  });
+
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+
+    }
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
+
+  useEffect(() => {
+    var widthIsShorter: boolean = 2 * dimensions.width < dimensions.height;
+
+    if (widthIsShorter) {
+      if (dimensions.width <= 600) {
+        document.documentElement.style.setProperty('--bgWidth', dimensions.width * (4 / 3) + 'px');
+        document.documentElement.style.setProperty('--playFieldWidth', dimensions.width * 1 + 'px');
+        document.documentElement.style.setProperty('--playFieldHeight', dimensions.width * 2 + 'px');
+      } else {
+        document.documentElement.style.setProperty('--bgWidth', dimensions.width * (4 / 3) * 0.7 + 'px');
+        document.documentElement.style.setProperty('--playFieldWidth', dimensions.width * 0.7 + 'px');
+        document.documentElement.style.setProperty('--playFieldHeight', dimensions.width * 1.4 + 'px');
+      }
+    } else {
+      if (dimensions.width <= 600) {
+        document.documentElement.style.setProperty('--bgWidth', dimensions.height * (2 / 3) + 'px');
+        document.documentElement.style.setProperty('--playFieldWidth', dimensions.height * 0.5 + 'px');
+        document.documentElement.style.setProperty('--playFieldHeight', dimensions.height + 'px');
+      } else {
+        document.documentElement.style.setProperty('--bgWidth', dimensions.height * (4 / 3) * 0.45 + 'px');
+        document.documentElement.style.setProperty('--playFieldWidth', dimensions.height * 0.45 + 'px');
+        document.documentElement.style.setProperty('--playFieldHeight', dimensions.height * 0.9 + 'px');
+      }
+    }
+  }, [dimensions.width, dimensions.height])
 
 
   return (
     <div className="flex justify-center">
-      <div className=" flex dark:bg-slate-300 bg-slate-700 w-full sm:w-[600px] md:w-[700px] lg:w-[900px] h-[100vh]">
-        <StatusPanel />
-        <div className="w-2/3 h-[100vh] relative">
-          <BasicModal gameState={gameState} />
-          <PlayField />
+      <div
+        style={{ width: 'var(--bgWidth, 100px)' }}
+        className="dark:bg-slate-300 bg-slate-700 h-[100vh] flex flex-col justify-center">
+
+        <div style={{ width: 'var(--bgWidth)', height: 'var(--playFieldHeight)' }}
+        className="flex flex-row">
+          <StatusPanel />
+          <div
+          id="playField"
+          style={{ width: 'var(--playFieldWidth)', height: 'var(--playFieldHeight)' }} 
+          className="w-full relative border-2 ">
+            <BasicModal />
+            <PlayField />
+          </div>
+          <NextPanel />
         </div>
-        <NextPanel />
       </div>
     </div>
   );
