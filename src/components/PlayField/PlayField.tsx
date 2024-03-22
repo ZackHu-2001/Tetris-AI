dcf2f4fbcebd35d533f3aac70b475b5993845b05
kai-zong <zong.k@northeastern.edu>
2024-03-21 18:08:48 -0700

    Feat: Created shdow for predictable falling location

--- src/components/PlayField/PlayField.tsx
+++ src/components/PlayField/PlayField.tsx
@@ -7,6 +7,9 @@ const PlayField: React.FC<PlayFieldProps> = () => {
     const { setStatus, setModal, board, moveDown, moveLeft, moveRight, drop, clockWiseRotate, anticlockWiseRotate, keyBindings } = useGameBoard()
     const rowNum = 20;
     const colNum = 10;
+    const boradLength = Object.keys(board).length;
+    let counted = false;
+    let tetrominoCopy = [];
 
     const initializeGame = () => {};
     
@@ -52,21 +55,61 @@ const PlayField: React.FC<PlayFieldProps> = () => {
     return (
         <div className='absolute w-full h-full top-0 left-0 z-10 flex flex-col '>
             {board.map((row, rowIndex) => {
+                counted = false;
+                if (rowIndex < boradLength - tetrominoCopy.length){
                 return (<div style={{height: '5%'}} className='w-full flex ' key={rowIndex}>
                     {Array.from({ length: colNum }).map((_, i) => {
                         return (
                             <div className=' h-full bg-black' key={rowIndex * 10 + i} 
-                            style={{ width: '10%', border: '', boxSizing: 'border-box', backgroundColor: (row & (1 << i)) ? "red" : ""}}>
+                            style={{ width: '10%', border: '', boxSizing: 'border-box', backgroundColor: (()=>{
+                                if (row & (1 << i)){
+                                    if (!counted){
+                                        counted = true;
+                                        tetrominoCopy.push(row);
+                                    }
+                                    return 'red'
+                                } else {
+                                    return ''
+                                }
+                            })()}}>
+                                {/* {row & (1 << i)} */}
+                            </div>
+                        )
+
+                    })}
+                </div>)}
+                else {
+                    return (<div style={{height: '5%'}} className='w-full flex ' key={rowIndex}>
+                    {Array.from({ length: colNum }).map((_, i) => {
+                        return (
+                            <div className=' h-full bg-black' key={rowIndex * 10 + i} 
+                            style={{ width: '10%', border: '', boxSizing: 'border-box', backgroundColor: (()=>{
+                                if (row & (1 << i)){
+                                    if (!counted){
+                                        counted = true;
+                                        tetrominoCopy.push(row);
+                                    }
+                                    return 'red'
+                                } else if (tetrominoCopy[rowIndex - boradLength + tetrominoCopy.length] & (1 << i)){
+                                    return 'lightcoral'
+                                }
+                                else {
+                                    return ''
+                                }
+                            })()}}>
                                 {/* {row & (1 << i)} */}
                             </div>
                         )
 
                     })}
                 </div>)
+                }
             }
             )}
         </div>
     )
 
 }
+
+
 export default PlayField;
